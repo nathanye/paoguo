1bfe29dead0041c2d40d94b3f31d5bcc4a914802
John Quicker <johnquicker@163.com>
2017-05-27 18:30:47 +0800

    try to convert html to markdown but it didnot touch html's ruby statements

--- app/controllers/pp1as_controller.rb
+++ app/controllers/pp1as_controller.rb
@@ -9,6 +9,35 @@ class Pp1asController < ApplicationController
     @pp1a = Pp1a.new(pp1a_params)
     @pp1a.pp1 = @pp1
     @pp1a.user = current_user
+    # require 'html_massage'
+
+  html = %{
+    <html>
+      <head>
+        <script type="text/javascript">document.write('I am a bad script');</script>
+      </head>
+      <body>
+      <p>
+        <%= @pp1.date %> <%= @pp1.week %><%= @pp1.yinli %>，<%= @pp1.weather %>
+        <br>
+        <%= @pp1.begin_time %>出门<%= @pp1.finish_time %>进门。
+        <br>
+        从<%= @pp1.from_where %><%= @pp1.how_to_go %>到<%= @pp1.main_place %>，<%= @pp1.run_begin_time %>到<%= @pp1.run_finish_time %>之间，<%= @pp1.how_long_time %>跑了<%= @pp1.how_long_kilos %>千米。<%= @pp1.stretch_or_not %>拉伸，<%= @pp1.how_to_go_back %>回<%= @pp1.back_to_where %>。
+  <br>
+        人：<%= @pp1.about_people %>
+  <br>
+        想：<%= @pp1.about_ideas %>
+  <br>
+        遇：<%= @pp1.about_serendipities %>
+  <br>
+        它：<%= @pp1.about_others %>
+
+       </p>
+      </body>
+    </html>
+  }
+  @pp1a.content = ReverseMarkdown.convert html
+  # => "This is some _great_ content!"
     if @pp1a.save
       redirect_to pp1_pp1a_path(@pp1a)
     else
