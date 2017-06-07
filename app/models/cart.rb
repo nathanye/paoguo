ff0745e2ac7b7b548b437dc6b17ffd32448a7ffb
John Quicker <johnquicker@163.com>
2017-06-07 17:49:11 +0800

    cart price now is cart_item.pp1.product.price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,18 @@
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :pp1s, through: :cart_items, source: :pp1
+  has_many :products, through: :cart_items, source: :product
 
   def add_pp1_to_cart(pp1)
     ci = cart_items.build
     ci.pp1 = pp1
+    ci.quantity = 1
+    ci.save
+  end
+
+  def add_product_to_cart(product)
+    ci = cart_items.build
+    ci.product = product
     ci.quantity = 1
     ci.save
   end
