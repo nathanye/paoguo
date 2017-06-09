1a2fbabb1bad420714f2174062ff5864ed2c5ba6
nathanye <khalil54781@gmail.com>
2017-06-09 14:51:55 +0800

    add qiniu messages

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["qiniu_access_key"]
-  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
-  config.qiniu_bucket        = ENV["qiniu_bucket"]
-  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
+  config.qiniu_access_key    = ENV["24UQ6YMdm_UnaO7njzEq_Fd1t_ISb65NuTKLVfV2"]
+  config.qiniu_secret_key    = ENV["nCnW4wRO0TQWlo3sphD_rVOxKzSuMXidp6NBSVCa"]
+  config.qiniu_bucket        = ENV["paoguo"]
+  config.qiniu_bucket_domain = ENV["oqa1xqxil.bkt.clouddn.com"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
